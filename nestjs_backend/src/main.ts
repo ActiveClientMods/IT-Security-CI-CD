a52657442ae9ad125c302e358754c623a5b5164b
Niklas Metzger <metzger989@gmail.com>
2025-10-25 15:59:53 +0200

    Fixed Issues. Adjusted dependencies & updated Gitleaks.

--- nestjs_backend/src/main.ts
+++ nestjs_backend/src/main.ts
@@ -2,12 +2,6 @@ import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-// SECURITY ISSUE: Hardcoded credentials (will be detected by Gitleaks)
-const DB_PASSWORD = "MySecretP@ssw0rd123!";
-const AWS_SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
-
-console.log(AWS_SECRET_KEY);
-
 export async function bootstrap() {
   const logger = new Logger("Main-Application");
 
@@ -16,8 +10,6 @@ export async function bootstrap() {
   await app.listen(port);
 
   logger.debug(`NestJS Server is running on Port: ${port}`);
-  // Using hardcoded credentials - BAD PRACTICE!
-  logger.warn(`Database password: ${DB_PASSWORD.substring(0, 3)}***`);
 }
 
 bootstrap().catch(err => console.error(err));
